a845d8ef4832fa9f82e0dcd369b3fcf28c8480d3
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 14:57:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,15 @@
 class Application
 
-def call (env)
-  resp =  Rack::Response.new
+  def call (env)
+    resp =  Rack::Response.new
 
-  t = Time.new
-if t.hour < 12
-  resp.write "Good Morning!"
-else
-  resp.write "Good Afternoon!"
-end
-
-end
+    t = Time.new
+    if t.hour < 12
+      resp.write "Good Morning!"
+      else
+      resp.write "Good Afternoon!"
+    end
+  end
 
 
 end
